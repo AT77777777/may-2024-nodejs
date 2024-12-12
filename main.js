c0b6fda83704f3445db73c3193aceff73f55e754
AT77777777 <aat3337777@gmail.com>
2024-12-12 12:45:57 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,19 @@
+const http = require('node:http');
+
+const foo = async () => {
+    const server = http.createServer((req, res) => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+
+        const port = server.address().port;
+
+        res.end(JSON.stringify({
+            data: `Live server successfully started on port ${port}`,
+        }));
+    });
+
+    server.listen(8000, () => {
+        console.log(`Server is running on port 8000`);
+    });
+};
+
+foo();
